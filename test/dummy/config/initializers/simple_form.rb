dee2c009727cb156e0facb770729db68cf1affe2
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-03 12:12:48 +0200

    localisation du formulaire

--- test/dummy/config/initializers/simple_form.rb
+++ test/dummy/config/initializers/simple_form.rb
@@ -134,7 +134,7 @@ SimpleForm.setup do |config|
   # config.default_input_size = 50
 
   # When false, do not use translations for labels.
-  # config.translate_labels = true
+  config.translate_labels = true
 
   # Automatically discover new inputs in Rails' autoload path.
   # config.inputs_discovery = true

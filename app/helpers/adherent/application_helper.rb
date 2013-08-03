4f5393ee739043c93a012c679e9beb82dfceef56
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-03 11:58:24 +0200

    modification des vues, importation icones pour utiliser icon_to, mise en place simple form

--- app/helpers/adherent/application_helper.rb
+++ app/helpers/adherent/application_helper.rb
@@ -1,4 +1,16 @@
 module Adherent
   module ApplicationHelper
+    
+    
+    def icon_to(icon_file, options={}, html_options={})
+    raise ArgumentError unless icon_file
+    title = alt = icon_file.split('.')[0].capitalize
+
+    html_options[:title] ||=title
+    html_options[:class] ||= 'icon_menu'
+    # html_options[:tabindex]= "-1"
+    img_path="adherent/icones/#{icon_file}"
+    link_to image_tag(img_path, :alt=> alt), options, html_options
+  end
   end
 end

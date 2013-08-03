88d82dd2e8c731dd74a962a394a41d8164bf4fad
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-03 12:15:35 +0200

    localisation et mise en place des marques obligatoire dans le form

--- app/models/adherent/adherent.rb
+++ app/models/adherent/adherent.rb
@@ -1,5 +1,7 @@
 module Adherent
   class Adherent < ActiveRecord::Base
     attr_accessible :birthdate, :forname, :name, :number
+    
+    validates :name, :number, :forname, :presence=>true
   end
 end

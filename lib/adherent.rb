8385cd27fc20864ae701877b38f28a492f4dd9ae
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-05 09:33:20 +0200

    tentative de modif des views templates

--- lib/adherent.rb
+++ lib/adherent.rb
@@ -1,4 +1,11 @@
 require "adherent/engine"
 
 module Adherent
+  class Engine < Rails::Engine
+    
+    config.app_generators do |g|
+      g.templates.unshift File::expand_path('../templates', __FILE__)
+    end
+    
+  end
 end
--- lib/adherent/engine.rb
+++ lib/adherent/engine.rb
@@ -2,9 +2,7 @@ module Adherent
   class Engine < ::Rails::Engine
     isolate_namespace Adherent
     
-  config.app_generators do |g|
-    g.templates.unshift File::expand_path('../templates', __FILE__)
-  end
+  
 
   end
 end

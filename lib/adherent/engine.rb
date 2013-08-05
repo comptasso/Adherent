c41a2253d7b2bd2ca5de2b16b95cea70f3b14a3e
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-05 09:26:44 +0200

    ajout de la suppression du path vers le template

--- lib/adherent/engine.rb
+++ lib/adherent/engine.rb
@@ -1,5 +1,10 @@
 module Adherent
   class Engine < ::Rails::Engine
     isolate_namespace Adherent
+    
+  config.app_generators do |g|
+    g.templates.unshift File::expand_path('../templates', __FILE__)
+  end
+
   end
 end

b2ea4e730b67d00f424fea3972254417fcd52b24
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-07 18:10:16 +0200

    mise en place d une validation chronologique des adhesions

--- app/models/adherent/adhesion.rb
+++ app/models/adherent/adhesion.rb
@@ -7,9 +7,17 @@ module Adherent
     
     belongs_to :member
     validates :from_date, :to_date, :member_id, presence:true
+    validate :chrono_order
     
     pick_date_for :from_date, :to_date
     
-  
+    
+    
+    protected
+    
+    def chrono_order
+      return unless (from_date && to_date) # on doit avoir les deux dates
+      errors.add(:from_date, 'la date de début ne peut être après la date de fin') if read_attribute(:from_date) > read_attribute(:to_date)
+    end
   end
 end
--- test/unit/adherent/adhesion_test.rb
+++ test/unit/adherent/adhesion_test.rb
@@ -1,9 +1,19 @@
+# coding utf-8
+
 require 'test_helper'
 
 module Adherent
   class AdhesionTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test 'une adhésion valide' do
+      @ad = Adhesion.new(from_date:'01/08/2013', to_date:'31/08/2013')
+      @ad.member_id = 1
+      assert_equal true, @ad.valid?
+    end
+    
+    test 'from_date doit être avant to_date' do
+      @ad = Adhesion.new(from_date:'01/09/2013', to_date:'31/08/2013')
+      @ad.member_id = 1
+      assert_equal false, @ad.valid?
+    end
   end
 end

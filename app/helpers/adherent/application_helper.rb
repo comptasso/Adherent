796fb8b6fd9d026506f491898e55689c836a5c99
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-08 07:33:05 +0200

    mise en place localisation fr dans l application dummy pour avoir number_to_currency opérationnel

--- app/helpers/adherent/application_helper.rb
+++ app/helpers/adherent/application_helper.rb
@@ -3,14 +3,19 @@ module Adherent
     
     
     def icon_to(icon_file, options={}, html_options={})
-    raise ArgumentError unless icon_file
-    title = alt = icon_file.split('.')[0].capitalize
+      raise ArgumentError unless icon_file
+      title = alt = icon_file.split('.')[0].capitalize
 
-    html_options[:title] ||=title
-    html_options[:class] ||= 'icon_menu'
-    # html_options[:tabindex]= "-1"
-    img_path="adherent/icones/#{icon_file}"
-    link_to image_tag(img_path, :alt=> alt), options, html_options
-  end
+      html_options[:title] ||=title
+      html_options[:class] ||= 'icon_menu'
+      # html_options[:tabindex]= "-1"
+      img_path="adherent/icones/#{icon_file}"
+      link_to image_tag(img_path, :alt=> alt), options, html_options
+    end
+    
+    # Pour transformer un montant selon le format numérique français avec deux décimales
+    def virgule(montant)
+      ActionController::Base.helpers.number_with_precision(montant, precision:2) rescue '0,00'
+    end
   end
 end

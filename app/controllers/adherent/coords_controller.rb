38d673554475db686310cccaef0d158f4e82b71d
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-22 14:35:54 +0200

    mise en place des spec pour le controller Coords

--- app/controllers/adherent/coords_controller.rb
+++ app/controllers/adherent/coords_controller.rb
@@ -79,17 +79,7 @@ module Adherent
       end
     end
   
-    # DELETE /coords/1
-    # DELETE /coords/1.json
-    def destroy
-      @coord = Coord.find(params[:id])
-      @coord.destroy
-  
-      respond_to do |format|
-        format.html { redirect_to coords_url }
-        format.json { head :no_content }
-      end
-    end
+    
     
     protected 
     
---
+++ spec/controllers/coords_controller_spec.rb
@@ -0,0 +1,129 @@
+# coding utf-8
+
+require 'spec_helper'
+
+describe Adherent::CoordsController do
+  
+  before(:each) do
+   @routes = Adherent::Engine.routes
+   @member = mock_model(Adherent::Member)
+   Adherent::Member.stub(:find).with(@member.to_param).and_return @member
+  end
+    
+  
+  describe "GET index" do
+    
+    
+    
+    it 'rend la vue index' do
+      get :index, member_id:@member.to_param
+      response.should render_template("index")
+    end
+    
+
+    it "assigns all coords as @coords" do
+      Organism.any_instance.should_receive(:members).and_return(@ar = double(Arel))
+      @ar.should_receive(:collect).and_return [1,2]
+      get :index, member_id:@member.to_param
+      assigns(:coords).should == [1,2]
+  
+    end
+  end
+  
+  describe "GET show" do
+    it 'appelle le coord' do
+      @member.should_receive(:coord).and_return(mock_model(Adherent::Coord))
+      get :show, member_id:@member.to_param
+    end
+    
+    it 'assigne le membre et rend la vue' do
+      @member.stub(:coord).and_return(@coord = mock_model(Adherent::Coord))
+      get :show, member_id:@member.to_param
+      assigns[:coord].should == @coord
+      response.should render_template('show')
+    end
+  end
+  
+  describe 'GET new' do
+    
+    it 'assigne un coord et rend la vue new' do
+      @member.should_receive(:build_coord).and_return(@coord = mock_model(Adherent::Coord))
+      get :new, member_id:@member.to_param
+      assigns[:coord].should == @coord
+      response.should render_template('new')
+    end
+    
+  end
+  
+  describe "GET edit" do
+    it 'appelle le coord' do
+      @member.should_receive(:coord).and_return(@coord = mock_model(Adherent::Coord))
+      get :edit, member_id:@member.to_param
+    end
+    
+    it 'assigne le membre et rend la vue' do
+      @member.stub(:coord).and_return(@coord = mock_model(Adherent::Coord))
+      get :edit, member_id:@member.to_param
+      assigns[:coord].should == @coord
+      response.should render_template('edit')
+    end
+  end
+  
+  describe "POST create" do
+    
+    before(:each) do
+      @attrib = {'city'=>'Bruxelles', 'zip'=>'1000'}
+    end
+    
+    it 'redirige vers new adhesion en cas de succès' do
+      @member.should_receive(:build_coord).with(@attrib).and_return(@coord = mock_model(Adherent::Coord))
+      @coord.should_receive(:save).and_return true
+      post :create, member_id:@member.to_param, :coord=>@attrib
+      response.should redirect_to(new_member_adhesion_url(assigns[:member]))
+    end
+    
+    it 'et vers la vue new autrement' do
+      @member.should_receive(:build_coord).with(@attrib).and_return(@coord = mock_model(Adherent::Coord))
+      @coord.should_receive(:save).and_return false
+      post :create, member_id:@member.to_param, :coord=>@attrib
+      response.should render_template 'new'
+    end
+    
+       
+  end
+  
+  describe "POST update" do
+    
+    before(:each) do
+      @coord = mock_model(Adherent::Coord)
+      @member.stub(:coord).and_return @coord
+    end
+    
+    it 'appelle update_attributes' do
+      @coord.should_receive(:update_attributes).with({'city'=>'Dallas'}).and_return true
+      post :update, {member_id:@member.to_param, :coord=>{:city=>'Dallas'} }
+    end
+    
+    it 'redirige vers show en cas de succès' do
+      @coord.stub(:update_attributes).with({'city'=>'Dallas'}).and_return true
+      post :update, {member_id:@member.to_param, :coord=>{:city=>'Dallas'} }
+      response.should redirect_to(member_coord_url(assigns[:member]))
+    end
+    
+    it 'et vers la vue edit autrement' do
+      @coord.stub(:update_attributes).with({'city'=>'Dallas'}).and_return false
+      post :update, {member_id:@member.to_param, :coord=>{:city=>'Dallas'} }
+      response.should render_template 'edit'
+    end
+    
+    it 'la variable @member est assignée' do
+      @coord.stub(:update_attributes).with({'city'=>'Dallas'}).and_return false
+      post :update, {member_id:@member.to_param, :coord=>{:city=>'Dallas'} }
+      assigns[:coord].should == @coord
+    end
+  end
+
+ 
+  
+  
+end

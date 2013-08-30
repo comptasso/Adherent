8a613d4908ad9a0cfe1cd54e90229eae8785e669
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-30 12:54:58 +0200

    mise en place de datatable pour la vue membres

--- app/assets/javascripts/adherent/members.js
+++ app/assets/javascripts/adherent/members.js
@@ -1,2 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var frenchdatatable = {
+    "sProcessing":   "Traitement en cours...",
+    "sLengthMenu":   "Afficher _MENU_ éléments",
+    "sZeroRecords":  "Aucun élément à afficher",
+    "sInfo":         "Affichage de l'élement _START_ à _END_ sur _TOTAL_ éléments",
+    "sInfoEmpty":    "Affichage de l'élement 0 à 0 sur 0 éléments",
+    "sInfoFiltered": "(filtré de _MAX_ éléments au total)",
+    "sInfoPostFix":  "",
+    "sSearch":       "Rechercher :",
+    "sUrl":          "",
+    "oPaginate": {
+        "sFirst":    "Premier",
+        "sPrevious": "Précédent",
+        "sNext":     "Suivant",
+        "sLast":     "Dernier"
+    }
+}
+
+$(document).ready( function () {
+    $('#members_id').dataTable({
+      "oLanguage": frenchdatatable
+    });
+} );

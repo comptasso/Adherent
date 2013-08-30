5133725c20c65ec5f60a137df456a6a48210884d
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2013-08-30 17:54:23 +0200

    capacité des tables à trier les colonnes de montants numériques

--- app/assets/javascripts/adherent/members.js
+++ app/assets/javascripts/adherent/members.js
@@ -26,7 +26,11 @@ $(document).ready( function () {
                 "sType": "date-euro",
                 "asSortable": ['asc', 'desc'],
                 "aTargets": ['date-euro'] // les colonnes date au format français ont la classe date-euro
-            }],
+            },
+            {
+                "bSortable": false,
+                "aTargets": ['actions' ]
+            }]          
     });
 } );
 
@@ -58,3 +62,57 @@ jQuery.fn.dataTableExt.oSort['date-euro-desc'] = function(a, b) {
                         var z = ((x < y) ? 1 : ((x > y) ? -1 : 0));
                         return z;
                 };
+
+// Gestion du tri des colonnes de montant
+// fonctions de tri pour des nombres avec une virgule comme séparateur décimal
+    jQuery.fn.dataTableExt.oSort['numeric-comma-asc']  = function(a,b) {
+    var x = (a == "-") ? 0 : a.replace( /,/, "." );
+    var y = (b == "-") ? 0 : b.replace( /,/, "." );
+    x = parseFloat( x );
+    y = parseFloat( y );
+    return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+};
+
+jQuery.fn.dataTableExt.oSort['numeric-comma-desc'] = function(a,b) {
+    var x = (a == "-") ? 0 : a.replace( /,/, "." );
+    var y = (b == "-") ? 0 : b.replace( /,/, "." );
+    x = parseFloat( x );
+    y = parseFloat( y );
+    return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
+
+
+
+//plug in permettant d'ajouter le type comma-decimals aux colonnes'
+// cette méthode qui pourrait être utilisée pour les dates 
+// permet à datatable d'identifier de lui même le type de la colonne
+jQuery.fn.dataTableExt.aTypes.unshift(
+    function ( sData )
+    {
+        var sValidChars = "0123456789-,";
+        var Char;
+        var bDecimal = false;
+
+        /* Check the numeric part */
+        for ( i=0 ; i<sData.length ; i++ )
+        {
+            Char = sData.charAt(i);
+            if (sValidChars.indexOf(Char) == -1)
+            {
+                return null;
+            }
+
+            /* Only allowed one decimal place... */
+            if ( Char == "," )
+            {
+                if ( bDecimal )
+                {
+                    return null;
+                }
+                bDecimal = true;
+            }
+        }
+
+        return 'numeric-comma';
+    }
+);

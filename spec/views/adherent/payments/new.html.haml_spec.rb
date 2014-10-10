12a95740a1e44edb01e2b5ba4a09995507716a3f
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-10-10 07:09:37 +0200

    tentative pour tester la vue

--- spec/views/adherent/payments/new.html.haml_spec.rb
+++ spec/views/adherent/payments/new.html.haml_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 
 describe "adherent/payments/new", :type => :view do
+  routes {Adherent::Engine.routes}
   
   before(:each) do
-    @routes = Adherent::Engine.routes
+    
     assign(:payment, double(Adherent::Payment))
     assign(:member, double(Adherent::Member, name:'Dupont', forname:'Jules'))
-#    allow(view).to receive(:member_payments_path).and_return 'bonjour'
-#    allow(view).to receive(:icon_to).and_return 'bonjour'
-#    allow(view).to receive(:member_payment_path).and_return 'bonjour'
+   # allow(view).to receive(:member_payments_path).and_return 'bonjour'
+   # allow(view).to receive(:icon_to).and_return 'bonjour'
+   # allow(view).to receive('member_payments_path').and_return 'bonjour'
   end
   
   it 'rend la vue' do
-    # render
+    render 
   end
 
 end

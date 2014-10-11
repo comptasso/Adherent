2f3ea1b0f210f883234fe4838a6b8afe411310c3
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2014-10-11 06:32:56 +0200

    explication changt de version

--- lib/adherent/version.rb
+++ lib/adherent/version.rb
@@ -2,5 +2,5 @@ module Adherent
   # VERSION = "0.1.6" # ajout de limite de dates à la saisie des paiements
   # VERSION = "0.1.7" # on ramène le gem Sass en dessous de la version 3.3 car e
   # erreur de compilation des assets sur Heroku
-  VERSION = "0.1.8" # passage à Rails 4 
+  VERSION = "0.1.8" # passage à RSpec 3 et ajout d'une fonctionnalité reçu 
 end

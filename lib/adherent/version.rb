18b41ad6043122307db3226a05b422cc0a8c0f40
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2015-02-23 21:22:58 +0100

    correction french_decimal_input

--- lib/adherent/version.rb
+++ lib/adherent/version.rb
@@ -8,6 +8,8 @@ module Adherent
   # VERSION = "0.2.0" # passage à rails 4.0 et bootstrap 3 
   # VERSION = "0.2.1" # reconstruction du gemfile.lock 
   # VERSION = "0.2.1.1" # correction coquille dans demo_table.scss 
-  VERSION = "0.2.2" # refonte des formulaires suites bootstrap 3 
+  # VERSION = "0.2.2" # refonte des formulaires suites bootstrap 3 
+  # VERSION = "0.2.2.1" # ajustements suite premiers essais staging 
+  VERSION = "0.2.2.2" # correction french_decimal_input 
   
 end

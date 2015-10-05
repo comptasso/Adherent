cf923f8332c6eace34b86774b3e20e44e822f5ff
Jean Claude Lepage <jean-claude.lepage@m4x.org>
2015-10-05 19:12:19 +0200

    passage à version 0.3.4

--- lib/adherent/version.rb
+++ lib/adherent/version.rb
@@ -18,5 +18,7 @@ module Adherent
   # VERSION = '0.2.9' # Mémorisation choix longueur liste des dataTables
   # VERSION = '0.3.0' # Passage à Rails 4.1
   # VERSION = '0.3.1' # Modifications pour le passage de flc à noschema
-  VERSION = '0.3.2' # find_member est maintenant défini dans ApplicationController
+  # VERSION = '0.3.2' # find_member est maintenant défini dans ApplicationController
+  # VERSION = '0.3.3' # retour en arrière car find_member(id) et find_member(member_id)
+  VERSION = '0.3.4' # les règlements sans adhésion s'affichent
   end
